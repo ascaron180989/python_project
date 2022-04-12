331c823ee47e4a0528b6a77b3d623538dcced3bc
Vyacheslav Ershov <94031513+ascaron180989@users.noreply.github.com>
2022-04-12 14:15:26 +0800

    Create config.py

---
+++ CurrencyConverterBot/config.py
@@ -0,0 +1,8 @@
+# Токен используемого telegram бота
+TOKEN = ''
+
+# URL API валютной биржи
+URL = 'https://min-api.cryptocompare.com/data/price'
+
+# Словарь доступных для конвертации валют
+values = {'евро': 'EUR', 'доллар': 'USD', 'рубль': 'RUB', 'юань': 'CNY'}
