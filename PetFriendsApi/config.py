c75a8b5452d4b65211e4b773214b2895a4601afa
Vyacheslav Ershov <94031513+ascaron180989@users.noreply.github.com>
2022-04-17 19:00:14 +0800

    Create config.py

---
+++ PetFriendsApi/config.py
@@ -0,0 +1,28 @@
+# URL сайта petfriends
+base_url = 'https://petfriends1.herokuapp.com'
+
+# Действительные email и password
+valid_email = 'ascaron1990@bk.ru'
+valid_password = 'stenlirus'
+
+# Не действительные email и password
+not_valid_email = 'ascaron@bk.ru'
+not_valid_password = 'stenli'
+
+# Данные тестового питомца 1
+name_1 = 'Tom'
+animal_type_1 = 'Cat'
+age_1 = 5
+pet_photo_url_1 = './images/tom.jpg'
+
+# Данные тестового питомца 1
+name_2 = 'Bob'
+animal_type_2 = 'Dog'
+age_2 = 3
+pet_photo_url_2 = './images/bob.jpeg'
+
+# Данные питомца 3 (все данные неверных типов)
+name_3 = 123
+animal_type_3 = 456
+age_3 = -100
+pet_photo_url_3 = './images/tim.txt'
